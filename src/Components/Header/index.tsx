d7ea5ffee578ab8cc053af4636c391b0de362ec4
Bruna Santiago <brunasanthiago@outlook.com>
2024-03-20 17:59:12 -0300

    refatorando o header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,19 +1,32 @@
-import { NavLink } from "react-router-dom";
-import { ContainerHeader, ContainerImg } from "./style";
+
+import { Link } from "react-router-dom";
+import { ContainerHeader, ContainerImg, ListaMenu, ContainerMenu } from "./style";
+import menu from "./menu.svg"
 
 export function Header() {
+
+
     return (
         <ContainerHeader>
+            
+            <ContainerMenu>
+                <input type="checkbox" id="check" />
+                <label htmlFor="check">
+                    <span>
+                        <img src={menu} alt="Menu" />
+                    </span>
+                </label>
+                <ListaMenu className="lista-menu">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">Sobre</Link></li>
+                    <li><Link to="/projects">Projetos</Link></li>
+                    <li><Link to="/contact">Contato</Link></li>
+                </ListaMenu>
+            </ContainerMenu>
             <ContainerImg>
                 <img src="https://avatars.githubusercontent.com/u/62550730?v=4" alt="Foto Perfil" />
                 <h5>Bruna Santiago</h5>
             </ContainerImg>
-            <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">Sobre</NavLink>
-                <NavLink to="/projects">Projetos</NavLink>
-                <NavLink to="/contact">Contato</NavLink>
-            </nav>
         </ContainerHeader>
-    )
+    );
 }
--- src/Components/Header/style.ts
+++ src/Components/Header/style.ts
@@ -2,34 +2,22 @@ import styled from 'styled-components';
 
 export const ContainerHeader = styled.header`
     display: flex;
-    align-items: center;
     justify-content: space-between;
-    height: 80px;
-
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1;
+    height: 4rem;
+    padding: 0.5rem;
+    
     background-color: ${props => props.theme['white-coffe']};
 
-    nav {
+    div {
         display: flex;
-        gap:1rem;
-        margin-right: 20px;
-        a {
-            text-decoration: none;
-            color: ${props => props.theme['pastel-brown']};
-            font-size: 1rem;
-
-            &:hover {
-                color: ${props => props.theme['coffe']};
-            }
-
-            &.active {
-                color: ${props => props.theme['coffe']};
-            }
-            
-        }
-
-
+        align-items: center;
     }
-
 `;
 
 export const ContainerImg = styled.div`
@@ -37,22 +25,84 @@ export const ContainerImg = styled.div`
     align-items: center;
     margin-left: 20px;
     img {
-        width: 50px;
-        border-radius: 50%;
-        margin-right: 10px;
+            display: none;
     }
+
     h5 {
         font-size: 1rem;
         color: ${props => props.theme['pastel-brown']};
     }
 
-    @media (max-width: 768px) {
+    @media (min-width: 768px) {
         img {
-            display: none;
+            width: 3.125rem;
+            border-radius: 50%;
+            margin-right: 10px;
+            display: block;
         }
         h5 {
-            font-size: 0.8rem;
+            font-size: 1.2rem;
         }
     }
 
 `;
+
+export const ContainerMenu = styled.div`
+    input {
+        display: none;
+        &:checked ~ ul {
+            display: block;
+        }
+    }
+    span {
+        width: 1.5rem;
+        height: 1.5rem;
+        background-repeat: no-repeat;
+        background-position: center;
+        display: inline-block;
+        
+        img#bg {
+            width: 1rem;
+            padding: 1rem;
+        }
+    }
+
+    @media (min-width: 768px) {
+        span img {
+            display: none;
+        }
+    }
+`;
+
+export const ListaMenu = styled.ul`
+    display: none;
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    right: auto;
+    li {
+        padding: 1rem;
+        background-color: ${props => props.theme['white-coffe']};
+        list-style: none;
+        
+        text-transform: uppercase;
+        a {
+
+            color: ${props => props.theme['pastel-brown']};
+
+            &:hover {
+                color: ${props => props.theme['coffe']};
+            }
+        }
+    }
+
+    @media (min-width: 768px) {
+        display: flex;
+        position: static;
+        width: auto;
+        li {
+            padding: 0;
+            margin: 0 1rem;
+        }
+    }
+`
